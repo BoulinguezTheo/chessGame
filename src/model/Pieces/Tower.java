18495bfffc7f6b7356ba41462b7c214cbcbf1a51
Theo <119578352+BoulinguezTheo@users.noreply.github.com>
2023-01-15 18:54:06 +0100

    tower moves implementation fully working and refacctored

--- src/model/Pieces/Tower.java
+++ src/model/Pieces/Tower.java
@@ -16,6 +16,42 @@ public class Tower extends Piece {
     }
     @Override
     public ArrayList<int[]> getMovesPossible(ChessData pBoard){
-        return new ArrayList<int[]>();
+        ArrayList<int[]> movesAvailable = new ArrayList<>();
+        //1
+        int xIncrementation1 = 1;
+        int yIncrementation1 = 0;
+        towerMoves(pBoard, movesAvailable, xIncrementation1, yIncrementation1);
+        //2
+        int xIncrementation2 = -1;
+        towerMoves(pBoard, movesAvailable, xIncrementation2, yIncrementation1);
+        //3
+        int xIncrementation3 = 0;
+        int yIncrementation2 = 1;
+        towerMoves(pBoard, movesAvailable, xIncrementation3, yIncrementation2);
+        //4
+        int yIncrementation3 = -1;
+        towerMoves(pBoard, movesAvailable, xIncrementation3, yIncrementation3);
+        
+        return movesAvailable;
+    }
+
+    private void towerMoves(ChessData pBoard, ArrayList<int[]> pMovesAvailable, int pXIncrementation, int pYIncrementation){
+        int newXCor = super.xCor + pXIncrementation;
+        int newYCor = super.yCor + pYIncrementation;
+        while(newXCor >= 0 && newXCor <= 7 && newYCor >= 0 && newYCor <= 7){
+            if(pBoard.getCells()[newXCor][newYCor].getCellContent().equals(" ")){
+                pMovesAvailable.add(new int[]{newXCor, newYCor});
+            } else {
+                Piece encounteredPiece = pBoard.getPiece(new int[] {newXCor, newYCor});
+                if(encounteredPiece != null && !super.color.equals(encounteredPiece.getColor())){
+                    pMovesAvailable.add(new int[]{newXCor, newYCor});
+                    break;
+                } else {
+                    break;
+                }
+            }
+            newXCor += pXIncrementation;
+            newYCor += pYIncrementation;
+        }
     }
 }
